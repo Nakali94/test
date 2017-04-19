76879154dffb68deccc3758ced6dee590b9c32c0
AD\xstrj78 <xstrj78@vse.cz>
2017-04-19 15:53:22 +0200

    promazani nepotrebneho

--- src/test/java/vse/p4it478/r2017/ls/cv/template/browser/marker/WithSearch.java
+++
@@ -1,9 +0,0 @@
-package vse.p4it478.r2017.ls.cv.template.browser.marker;
-
-import vse.p4it478.r2017.ls.cv.template.browser.page.SearchResultPage;
-
-public interface WithSearch {
-
-	public SearchResultPage search(String text);
-
-}
